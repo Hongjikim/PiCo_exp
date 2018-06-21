33f7c50943d3cb9a19bb7034904c9cecbc5137f7
wanirepo <choongwan.woo@gmail.com>
2018-06-21 17:54:29 +0900

    updates

--- story_display/pico_fmri_generate_ts.m
+++ story_display/pico_fmri_generate_ts.m
@@ -4,13 +4,12 @@ stories = filenames(fullfile(subject_dir, '*.txt')); % story01.txt story02.txt
 
 %% story order (randomize)
 
-
-
+% reorder: stories 
 
 %% text_duration 
 
 for story_i = 1:numel(stories)
-    out = pico_text_duration(stories{story_i})
+    out{story_i} = pico_text_duration(stories{story_i});
 end
 
 %%
@@ -18,6 +17,16 @@ end
 % fprintf('\ntotal time: %.2f seconds \n', sum(duration(:,2)));
 % fprintf('total words: %.f words \n*************************\n', my_length);
 
+
+run_i = 1;
+ts{run_i}{1} = out{1};
+ts{run_i}{2} = out{2};
+
+run_i = 2;
+ts{run_i}{1} = out{3};
+ts{run_i}{2} = out{4};
+
 % save ts
+
 
 end
