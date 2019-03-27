1ff75e8011a71e9cf03fc5fbd38ac62278d1fee2
Hongji Kim <redkim94@hanmail.net>
2019-03-27 16:22:00 +0900

    updates

--- story_display/try_beep.m
+++
@@ -1,9 +0,0 @@
-    InitializePsychSound;
-        pahandle = PsychPortAudio('Open', [], 2, 0, 44100, 2);
-        % Sound recording: Preallocate an internal audio recording  buffer with a capacity of 10 seconds
-        PsychPortAudio('GetAudioData', pahandle, 10);
-
-
-   % beeping
-            beep = MakeBeep(1000,.2);
-            Snd('Play',beep);
